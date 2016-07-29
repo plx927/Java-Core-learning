e48ac85133dcffe26af538917e4a682e96952106
plx <784580872@qq.com>
2016-07-29 08:23:49 +0800

    NIO复习、JSSE学习

--- Java-Core-learning-networking/src/main/java/com/panlingxiao/networking/ssl/HttpsClient.java
+++ Java-Core-learning-networking/src/main/java/com/panlingxiao/networking/ssl/HttpsClient.java
@@ -1,23 +1,26 @@
 package com.panlingxiao.networking.ssl;
 
-import javax.net.SocketFactory;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.Socket;
 
 /**
  * Created by panlingxiao on 2016/7/29.
  * 使用SSLSocket发送Http请求
+ * 参考：https://docs.oracle.com/javase/8/docs/technotes/guides/security/jsse/samples/sockets/client/SSLSocketClient.java
+ *
+ * This example demostrates how to use a SSLSocket as client to
+ * send a HTTP request and get response from an HTTPS server.
+ * It assumes that the client is not behind a firewall
  */
 public class HttpsClient {
     static  SSLSocketFactory sslSocketFactory;
     static SSLSocket sslSocket;
     public static void main(String[] args) throws  Exception{
          sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-         sslSocket = (SSLSocket) sslSocketFactory.createSocket("www.baidu.com", 80);
+         sslSocket = (SSLSocket) sslSocketFactory.createSocket("www.verisign.com", 443);
 
         //客户端获取其所支持的加密套件
         String[] cipherSuites = sslSocket.getSupportedCipherSuites();
@@ -25,7 +28,7 @@ public class HttpsClient {
 
         //构造HTTP请求头
         StringBuffer httpRequest = new StringBuffer("GET / HTTP/1.1\n");
-        httpRequest.append("Host: www.baidu.com\n");
+        httpRequest.append("Host: www.verisign.com\n");
         httpRequest.append("Connection: keep-alive\n");
         httpRequest.append("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n");
         httpRequest.append("Upgrade-Insecure-Requests: 1\n");
