97e9afe3e842417f922949e2523aae7d89aac100
plx927 <784580872@qq.com>
2016-08-05 10:20:35 +0800

    JDBC复习

---
+++ Java-Core-learning-jdbc/src/main/java/com/panlingxiao/jdbc/JdbcTest.java
@@ -0,0 +1,14 @@
+package com.panlingxiao.jdbc;
+
+import java.sql.PreparedStatement;
+
+/**
+ * Created by panlingxiao on 2016/8/5.
+ */
+public class JdbcTest {
+    public static void main(String[] args) {
+        PreparedStatement psmt = null;
+        //psmt.addBatch();
+
+    }
+}
